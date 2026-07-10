561aa1160929d77dbfa40716a128accd017183cd
PK <priyankanwal967@gmail.com>
2026-07-10 19:26:25 +0530

    react_05 project :initial commit

---
+++ react_04/src/Components/Api.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Api() {
+
+  const 
+  useEffect(()=>{
+    let api = "";
+    fetch(api)
+    .then((res)=>{
+      return res.json()
+    })
+    .then(data) =>{
+      console.log(dat);
+      
+    }
+    .cath()
+  } , [])
+  return (
+    <div>Api</div>
+  )
+}
+
+export default Api
